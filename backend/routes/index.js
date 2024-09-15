34c0b0ece6b10050e6f20777f4c4b8ecb6e500bd
Evie Ng <yw.evie.ng@gmail.com>
2024-09-14 22:51:00 -0400

    Initialize Express and Sequelize with CSRF protection

---
+++ backend/routes/index.js
@@ -0,0 +1,24 @@
+// backend/routes/index.js
+const express = require('express');
+const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter)
+
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
+
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
+});
+
+router.use('/api', apiRouter);
+
+module.exports = router;
