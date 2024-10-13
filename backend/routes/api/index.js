14558377ea0c274b0b906f1dbdb53cb5b047b4b7
Evie Ng <yw.evie.ng@gmail.com>
2024-10-13 19:30:41 -0400

    Update readme with new project name

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -12,7 +12,8 @@ router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
-router.post('/test', (req, res) => {
+// Keep this route to test frontend setup in Mod 5
+router.post('/test', function (req, res) {
   res.json({ requestBody: req.body });
 });
 
