996c6f46d2696e03766c2f5e38c722b38baf1cbb
Evie Ng <yw.evie.ng@gmail.com>
2024-10-21 21:48:47 -0400

    Added login form page phase 1

---
+++ frontend/src/store/session.js
@@ -0,0 +1,142 @@
+import { csrfFetch } from './csrf';
+
+// constant to avoid debugging typos
+const ADD_SESSION = 'session/add_session';
+const REMOVE_SESSION = 'session/remove_session';
+
+
+// const SET_USER = "session/setUser";
+// const REMOVE_USER = "session/removeUser";
+
+// const setUser = (user) => {
+//   return {
+//     type: SET_USER,
+//     payload: user
+//   };
+// };
+
+// const removeUser = () => {
+//   return {
+//     type: REMOVE_USER
+//   };
+// };
+
+// export const login = (user) => async (dispatch) => {
+//   const { credential, password } = user;
+//   const response = await csrfFetch("/api/session", {
+//     method: "POST",
+//     body: JSON.stringify({
+//       credential,
+//       password
+//     })
+//   });
+//   const data = await response.json();
+//   dispatch(setUser(data.user));
+//   return response;
+// };
+
+// const initialState = { user: null };
+
+// const sessionReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case SET_USER:
+//       return { ...state, user: action.payload };
+//     case REMOVE_USER:
+//       return { ...state, user: null };
+//     default:
+//       return state;
+//   }
+// };
+
+// regular action creator
+const addSession = (user) => {
+  return {
+    type: ADD_SESSION,
+    user
+  };
+};
+
+export const removeSession = () => {
+  return {
+    type: REMOVE_SESSION,
+  }
+}
+
+// thunk action creator
+export const login = (user) => async (dispatch) => {
+  const { credential, password } = user;
+  const response = await csrfFetch('/api/session', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      credential,
+      password
+    }),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(addSession(data.user));
+    return response;
+  }
+}
+
+export const logout = () => async (dispatch) => {
+  const res = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+
+  if (res.ok) {
+    const result = await res.json();
+    dispatch(removeSession())
+    return result;
+  }
+}
+
+export const restoreUser = () => async (dispatch) => {
+  const response = await csrfFetch("/api/session");
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(addSession(data.user));
+    return response;
+  }
+  }
+
+export const signUp = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const res = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password}),
+  });
+
+  if (res.ok) {
+    const data = await res.json();
+    dispatch(addSession(data.user));
+    return res;
+  }
+};
+
+// reducer
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_SESSION: {
+      const newState = {...state, user: action.user};
+      return newState;
+    }
+    case REMOVE_SESSION: {
+      return {...state, user: null }
+    }
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,16 +1,18 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import sessionReducer from './session';
+import spotReducer from './spot';
+import reviewReducer from './review';
 
 const rootReducer = combineReducers({
-
+  // ADD REDUCERS HERE
+  session: sessionReducer,
+  spot: spotReducer,
+  review: reviewReducer
 });
 
-const configureStore = (preloadedState) => {
-    return createStore(rootReducer, preloadedState, enhancer);
-  };
-
 let enhancer;
-if (import.meta.env.MODE === 'production') {
+if (import.meta.env.MODE === "production") {
   enhancer = applyMiddleware(thunk);
 } else {
   const logger = (await import("redux-logger")).default;
@@ -18,5 +20,9 @@ if (import.meta.env.MODE === 'production') {
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
+
+const configureStore = (preloadedState) => {
+  return createStore(rootReducer, preloadedState, enhancer);
+};
 
 export default configureStore;
